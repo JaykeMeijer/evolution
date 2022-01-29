e37a78112d71fefbb4aec07820538832af08aca9
Jayke Meijer <jayke.meijer@gmail.com>
2022-01-29 21:14:37 +0100

    Some cleanup & maintenance

--- src/simulation/ui/ui.py
+++ src/simulation/ui/ui.py
@@ -1,19 +1,15 @@
 import math
-from typing import Any, Dict, Iterable, List, Tuple, cast
-from beast.brain.neuron import InputNeuron, InputType, InternalNeuron, Neuron, OutputNeuron, OutputType
+from typing import Dict, List, Tuple, cast
 
-import networkx as nx
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 import pygame
 
 from beast.beast import Beast
-from beast.brain.brain import Brain, BrainRenderer
+from beast.brain.brain import BrainRenderer
 from simulation.render_helpers import draw_multiline_text
 from simulation.ui_constants import XSIZE, YSIZE
 from simulation.ui.interactions import toggle_pause
-from simulation.ui.ui_elements import Button, Element, Image, Popup, ToggleButton
-from util.math_helpers import get_direction, translate
+from simulation.ui.ui_elements import Button, Element, Popup, ToggleButton
 from world.state import State
 
 mpl.use('Agg')
--- src/util/math_helpers.py
+++ src/util/math_helpers.py
@@ -3,12 +3,24 @@ from typing import Tuple
 
 
 def get_direction(from_point: Tuple[int, int], to_point: Tuple[int, int]) -> int:
+    """
+    Get the direction from one point to another. Is given as degrees, with 0/360 being up, 90 right, 180 down etc.
+
+    Note that this is in PyGame coordinates, with (0,0) being the top-left, meaning that the Y-direction is reversed
+    compared to mathmetical coordinates.
+    """
     return round(math.degrees(
         math.atan2((to_point[0] - from_point[0]), -(to_point[1] - from_point[1]))
     ))
 
 
 def translate(start: Tuple[int, int], direction: int, distance: int) -> Tuple[int, int]:
+    """
+    Determine coordinates at a certain distance and in a certain direction (in degrees) from the given coordinate.
+
+    Note that this is in PyGame coordinates, with (0,0) being the top-left, meaning that the Y-direction is reversed
+    compared to mathmetical coordinates.
+    """
     radians = math.radians(direction)
     return (
         round(start[0] + math.sin(radians) * distance),
