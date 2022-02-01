4a10a2804666970215012b4f17d31fc70f371ddd
Jayke Meijer <jayke.meijer@gmail.com>
2022-02-01 22:11:38 +0100

    Added simple profiling wrapper

---
+++ evolution/util/profiling.py
@@ -0,0 +1,15 @@
+import cProfile
+
+
+def profileit(name):
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            prof = cProfile.Profile()
+            retval = prof.runcall(func, *args, **kwargs)
+            # Note use of name from outer scope
+            prof.dump_stats("local/" + name)
+            return retval
+
+        return wrapper
+
+    return inner
